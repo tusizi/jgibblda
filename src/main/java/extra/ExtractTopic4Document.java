4fb6e6d3ab0f0b971bd3aa4c13953c108750fd72
tusizi <529340225@qq.com>
2016-10-07 13:14:58 +0800

    add weidaima to src/main/java/extra/ExtractTopic4Document.java

--- src/main/java/extra/ExtractTopic4Document.java
+++ src/main/java/extra/ExtractTopic4Document.java
@@ -13,6 +13,48 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ExtractTopic4Document {
+
+    /*
+        //读入theta文件
+        Input model-final.theta
+
+        //读入话题文件文件
+        Input model-final.twords
+
+         // 解析每行浮点数的权重值放到二维集合类里面
+        List<List<Double>> totalProbabilities <- extract model-final.theta
+
+        //解析每一个话题
+        List<List<Vocabulary>> articleTopics <- extract model-final.twords
+
+        //循环每一个文章的权重列表
+        for index, probabilities <- totalProbabilities {
+
+            //把一个文章里面的权重值从大到小排序
+            tempProbabilities <- sort probabilities
+
+            //获取最大的权重值
+            maxProbability <- tempProbabilities.max
+
+            // 获取最大的权重值在源列表中的位置
+            position <- probabilities.indexOf(maxProbability)
+
+            //通过当前的循环index和位置position得到当前文章的最关联的topic
+            topic <- articleTopics[index][position]
+
+            //输出当前文章的topic
+            Output topic
+        }
+
+        //话题里面每一个词的内容和权重的类
+        class Vocabulary {
+            具体的词汇名字
+            name
+
+            当前词在topic中的权重值
+            percent
+        }
+     */
     public static void main(String[] args) {
         Argument argument = new Argument();
         CmdLineParser parser = new CmdLineParser(argument);
